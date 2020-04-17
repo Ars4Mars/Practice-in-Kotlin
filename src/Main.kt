2b533ef55cd7cab3caf881065983b39efe847045
ArsMars <tag_mahal@mail.ru>
2020-04-17 23:31:17 +0300

    clean commit

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,9 @@ import kotlinx.coroutines.*
 var a = 0
 var b = 0
 var operation = 0
+var answer = 0
 val input = Scanner(System.`in`)
+
 fun main(args: Array<String>){
     GlobalScope.launch { // launch a new coroutine in background and continue
         delay(2000L) // non-blocking delay for 1 second (default time unit is ms)
@@ -14,23 +16,25 @@ fun main(args: Array<String>){
         //println(a + b)
         //var operation = 0
         println("Welcome to Kotlin calculator! \n choose operation: \n 1- plus \n 2-minus \n 3-multiplay \n 4-divide ")
-        InputNumber()
+        inputNumber()
     }
-fun InputNumber(){
+fun inputNumber(){
     println("Welcome to Kotlin calculator! \n \bchoose two numbers")
 
     a = input.nextInt()
     b = input.nextInt()
-    InputOper()
+    inputOper()
 }
 
-fun InputOper(){
+fun inputOper(){
      operation = input.nextInt()
     when (operation){
-        1-> print(a+b)
-        2-> print(a-b)
-        3-> print(a*b)
-        4-> print(a/b)
+        1-> answer = a+b
+        2-> answer = a-b
+        3-> answer = a*b
+        4-> answer = a/b
     }
+
+    println(answer)
 
 }
